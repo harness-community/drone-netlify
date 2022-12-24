b47cc512a186ae8516e1ed9d753d017dc2276983
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2022-12-24 13:57:37 -0500

    Update script.sh
    Added a few more CLI flags.

--- script.sh
+++ script.sh
@@ -19,16 +19,22 @@ then
      NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --debug ${PLUGIN_DEBUG}"
 fi
 
+#Netlify CI Build Flag
+if [ -n "$PLUGIN_BUILD" ]
+then
+     NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --build ${PLUGIN_BUILD}"
+fi
+
 #Message Flag
 if [ -n "$PLUGIN_MESSAGE" ]
 then
      NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --message ${PLUGIN_MESSAGE}"
 fi
 
-#Path Flag
-if [ -n "$PLUGIN_PATH" ]
+#Upload Location Dir Flag
+if [ -n "$PLUGIN_DIR" ]
 then
-    NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --dir ${PLUGIN_PATH}"
+    NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --dir ${PLUGIN_DIR}"
 else
     NETLIFY_DEPLOY_OPTIONS="${NETLIFY_DEPLOY_OPTIONS} --dir ./"
 fi
@@ -45,10 +51,14 @@ then
     fi
 fi
 
+#Output Info
+echo "> Netlify CLI and NodeJS Version: " && netlify -v 
+
 #Deploy Step
 if [ -n "$PLUGIN_SITE_ID" ] && [ -n "$PLUGIN_TOKEN" ]
 then
     NETLIFY_SITE="--auth $PLUGIN_TOKEN --site $PLUGIN_SITE_ID"
+    echo "> Executing: netlify deploy $NETLIFY_SITE $NETLIFY_DEPLOY_OPTIONS"
     echo "> Deploying on Netlify…" &&
     netlify deploy $NETLIFY_SITE $NETLIFY_DEPLOY_OPTIONS;
 else
@@ -63,5 +73,5 @@ then
     echo "> non-zero exit code $rc" &&
     exit $rc
 else
-    echo $'\n'"> Successfully deployed!"$'\n'
+    echo $'\n'"> Successfully Deployed!"$'\n'
 fi
